9ca26f60952e9c12d0be915964bdf60f8f8d2cd8
Abdulrajak s Mulla <112795885+itsmeabd85@users.noreply.github.com>
2025-07-07 22:41:02 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -2,10 +2,10 @@
 set -e
 
 # Pull the latest Docker image from Docker Hub
-docker pull abdulrajak/devops_cicd_task_falsk_app:latest
+docker pull abdulrajak/devops_cicd_task_flask_app:latest
 
 # Stop and remove any running containers based on that image
-old_container=$(docker ps -q --filter "ancestor=abdulrajak/devops_cicd_task_falsk_app:latest")
+old_container=$(docker ps -q --filter "ancestor=abdulrajak/devops_cicd_task_flask_app:latest")
 if [ ! -z "$old_container" ]; then
     echo "Stopping old container..."
     docker stop "$old_container"
@@ -14,5 +14,5 @@ fi
 
 # Start the new container on port 5001
 echo "Starting the container..."
-docker run -d -p 5001:5001 abdulrajak/devops_cicd_task_falsk_app:latest
+docker run -d -p 5001:5001 abdulrajak/devops_cicd_task_flask_app:latest
 echo "Container started successfully at http://localhost:5001"
