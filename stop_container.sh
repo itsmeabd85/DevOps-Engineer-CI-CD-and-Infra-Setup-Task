8d3768532edfe48a2a1336a8d5468b782ea3cfa7
Abdulrajak s Mulla <112795885+itsmeabd85@users.noreply.github.com>
2025-07-07 21:31:14 +0530

    Create stop_container.sh

---
+++ stop_container.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Stop and remove the container based on image
+container_id=$(docker ps -q --filter "ancestor=abdulrajak/devops_cicd_task_falsk_app:latest")
+
+if [ -z "$container_id" ]; then
+  echo "No running container found for this image."
+else
+  echo "Stopping container $container_id..."
+  docker stop "$container_id"
+  docker rm "$container_id"
+  echo "Container stopped and removed."
+fi
